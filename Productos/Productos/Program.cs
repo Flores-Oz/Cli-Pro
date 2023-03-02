6071f11f3ae2ef2bf6de7ca215f0a62c1ff5f0c6
Flores-Oz <oscrecreate@gmail.com>
2023-03-02 16:01:01 -0600

    Version 1

---
+++ Productos/Productos/Program.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos
+{
+    internal class Program
+    {
+        struct Producto
+        {
+            public double PrecioCosto, PreciodeVenta, Existencia;
+            public String Codigo, Descripcion;
+        }
+
+        static Producto[] misProductos = new Producto[10];
+        static int contador = 0;
+
+        /* Metodos */
+        public static void IngresarProductos() {
+            char siono = 's';
+            do
+            {
+                bool bandera = false;
+                string auxCod = "";
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Ingreso de un Nuevo Producto");
+                    Console.Write("Codigo: ");
+                    auxCod = Console.ReadLine();
+                    bandera = false;
+                    for (int i = 0; i < contador; i++)
+                    {
+                        if (misProductos[i].Codigo == auxCod)
+                        {
+                            bandera = true;
+                        }
+                    }
+                    if (bandera == true)
+                    {
+                        Console.WriteLine("Error: Este Codigo ya esta ingresado");
+                        Console.ReadKey();
+                        Console.Write("¿Desea intentar nuevamente? S/N: ");
+                        siono = Convert.ToChar(Console.ReadLine());
+                    }
+                } while (bandera == true && (siono == 's' || siono == 'S'));
+                if (bandera == false)
+                {
+                    misProductos[contador].Codigo = auxCod;
+                    Console.Write("Descripción: ");
+                    misProductos[contador].Descripcion = Console.ReadLine();
+                    Console.Write("Precio Costo: ");
+                    misProductos[contador].PrecioCosto = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Precio Venta: ");
+                    misProductos[contador].PreciodeVenta = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Existencia: ");
+                    misProductos[contador].Existencia = Convert.ToDouble(Console.ReadLine());
+                    contador++;
+                    Console.Write("¿Desea ingresar otro Producto? S/N: ");
+                    siono = Convert.ToChar(Console.ReadLine());
+                }
+
+            } while (siono == 's' || siono == 'S');
+        }
+
+        public static void MostrarProductos() 
+        {
+            Console.Clear();
+            Console.SetCursorPosition(5, 5);
+            Console.Write("Codigo");
+            Console.SetCursorPosition(15, 5);
+            Console.Write("Descripción");
+            Console.SetCursorPosition(35, 5);
+            Console.Write("Precio Costo");
+            Console.SetCursorPosition(55, 5);
+            Console.Write("Precio Venta");
+            Console.SetCursorPosition(75, 5);
+            Console.Write("Existencia");
+
+            for (int i = 0; i < contador; i++)
+            {
+                Console.SetCursorPosition(5, (6 + i));
+                Console.WriteLine(misProductos[i].Codigo);
+                Console.SetCursorPosition(15, (6 + i));
+                Console.WriteLine(misProductos[i].Descripcion);
+                Console.SetCursorPosition(35, (6 + i));
+                Console.WriteLine(misProductos[i].PrecioCosto);
+                Console.SetCursorPosition(55, (6 + i));
+                Console.WriteLine(misProductos[i].PreciodeVenta);
+                Console.SetCursorPosition(75, (6 + i));
+                Console.WriteLine(misProductos[i].Existencia);
+            }
+            Console.ReadLine();
+        }
+
+        public static void BuscarProductos() 
+        {
+            char siono;
+            do
+            {
+                Console.Clear();
+                siono = 'n';
+                int pos = -1;
+                do
+                {
+                    Console.Write("Codigo a Buscar: ");
+                    string codBuscar = Console.ReadLine();
+                    for (int i = 0; i < contador; i++)
+                    {
+                        if (misProductos[i].Codigo == codBuscar)
+                        {
+                            pos = i;
+                        }
+                    }
+                    if (pos == -1)
+                    {
+                        Console.WriteLine("Error: Este Codigo no Existe");
+                        Console.ReadKey();
+                        Console.Write("¿Desea intentar nuevamente? S/N: ");
+                        siono = Convert.ToChar(Console.ReadLine());
+                    }
+                } while (pos != -1 && (siono == 's' || siono == 'S'));
+                if (pos != -1)
+                {
+                    Console.WriteLine("Codigo: " + misProductos[pos].Codigo);
+                    Console.WriteLine("Descripción: "+ misProductos[pos].Descripcion);
+                    Console.WriteLine("Precio Costo: "+ misProductos[pos].PrecioCosto);
+                    Console.WriteLine("Precio Venta: "+ misProductos[pos].PreciodeVenta);
+                    Console.WriteLine("Existencia: "+ misProductos[pos].Existencia);
+                    Console.WriteLine("¿Desea Buscar otro Producto? S/N: ");
+                    siono = Convert.ToChar(Console.ReadLine());
+                }
+
+            } while (siono == 's' || siono == 'S');
+        }
+
+        public static void ModificarProductos() 
+        {
+            char siono;
+            do
+            {
+                Console.Clear();
+                siono = 'n';
+                int pos = -1;
+                do
+                {
+                    Console.Write("Ingrese el Codigo que Desea Modificar: ");
+                    string codBuscar = Console.ReadLine();
+                    for (int i = 0; i < contador; i++)
+                    {
+                        if (misProductos[i].Codigo == codBuscar)
+                        {
+                            pos = i;
+                        }
+                    }
+                    if (pos == -1)
+                    {
+                        /*Si no llegara a Encontrar el Producto da "Error"*/
+                        Console.WriteLine("Error: Este Codigo no Existe");
+                        Console.ReadKey();
+                        Console.Write("¿Desea intentar nuevamente? S/N: ");
+                        siono = Convert.ToChar(Console.ReadLine());
+                    }
+                } while (pos != -1 && (siono == 's' || siono == 'S'));
+                if (pos != -1)
+                {
+                    /* Presenta los Datos Actuales del Producto*/
+                    Console.SetCursorPosition(50, 3);
+                    Console.WriteLine("Datos del Producto Actual");
+                    Console.SetCursorPosition(50, 4);
+                    Console.WriteLine("Codigo: " + misProductos[pos].Codigo);
+                    Console.SetCursorPosition(50, 5);
+                    Console.WriteLine("Descripción: " + misProductos[pos].Descripcion);
+                    Console.SetCursorPosition(50, 6);
+                    Console.WriteLine("Precio Costo: " + misProductos[pos].PrecioCosto);
+                    Console.SetCursorPosition(50, 7);
+                    Console.WriteLine("Precio Venta: " + misProductos[pos].PreciodeVenta);
+                    Console.SetCursorPosition(50, 8);
+                    Console.WriteLine("Existencia: " + misProductos[pos].Existencia);
+
+                    /* Modifica los Datos Actuales del Producto*/
+                    Console.SetCursorPosition(0, 3);
+                    Console.Write("Nueva Descripciòn: ");
+                    misProductos[pos].Descripcion = Console.ReadLine();
+                    Console.Write("Nuevo Precio Costo: ");
+                    misProductos[pos].PrecioCosto = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Nuevo Precio Venta: " );
+                    misProductos[pos].PreciodeVenta = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Nueva Existencia: " );
+                    misProductos[pos].Existencia = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("¿Desea Modificar otro Producto? S/N: ");
+                    siono = Convert.ToChar(Console.ReadLine());
+                }
+
+            } while (siono == 's' || siono == 'S');
+        }
+    
+
+        public static void EliminarProductos() 
+        {
+            char siono;
+            do
+            {
+                Console.Clear();
+                siono = 'n';
+                int pos = -1;
+                do
+                {
+                    Console.Write("Ingrese el Codigo que Desea Eliminar: ");
+                    string codBuscar = Console.ReadLine();
+                    for (int i = 0; i < contador; i++)
+                    {
+                        if (misProductos[i].Codigo == codBuscar)
+                        {
+                            pos = i;
+                        }
+                    }
+                    if (pos == -1)
+                    {
+                        /*Si no llegara a Encontrar el Producto da "Error"*/
+                        Console.WriteLine("Error: Este Codigo no Existe");
+                        Console.ReadKey();
+                        Console.Write("¿Desea intentar nuevamente? S/N: ");
+                        siono = Convert.ToChar(Console.ReadLine());
+                    }
+                } while (pos != -1 && (siono == 's' || siono == 'S'));
+                if (pos != -1)
+                {
+                    char desicion = 's';
+                    Console.Write("¿Esta seguro de eliminar este Producto? S/N: ");
+                    desicion = Convert.ToChar(Console.ReadLine());
+                    if (desicion == 's' || desicion == 'S')
+                    {
+                        for (int x = pos; x < contador; x++)
+                        {
+                            misProductos[x] = misProductos[x + 1];
+                        }
+                        contador--;
+                        Console.WriteLine("Producto Eliminado con exito");
+                    }
+                    else { 
+                        Console.WriteLine("Producto no Eliminado");
+
+                    } 
+                    Console.WriteLine("¿Desea Eliminar otro Producto? S/N: ");
+                    siono = Convert.ToChar(Console.ReadLine());
+                }
+
+            } while (siono == 's' || siono == 'S');
+        }
+    
+
+        /* Menu */
+        public static void Menu()
+        {
+            bool salir = false;
+            while (!salir)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Ingresar Producto");
+                Console.WriteLine("2. Mostrar Productos");
+                Console.WriteLine("3. Buscar Producto por Codigo");
+                Console.WriteLine("4. Modificar Producto");
+                Console.WriteLine("5. Eliminar Producto");
+                Console.WriteLine("6. Salir");
+                Console.WriteLine("Elige una de las opciones");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        IngresarProductos();
+                        break;
+                    case 2:
+                        MostrarProductos();
+                        break;
+                    case 3:
+                        BuscarProductos();
+                        break;
+                    case 4:
+                        ModificarProductos();
+                        break;
+                    case 5:
+                        EliminarProductos();
+                        break;
+                    case 6:
+                        Console.WriteLine("Has elegido salir de la aplicación");
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Elige una opcion entre 1 y 5");
+                        break;
+                }
+
+            }
+        }
+    
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}
---
+++ clientes/clientes/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clientes
+{
+    internal class Program
+    {
+        struct Cliente
+        {
+            public String nit, nombre, apellido,telefono;
+            public DateTime fechaNacimiento;
+            public int edad;
+        }
+
+        public static void IngresarClientes()
+        {
+            Console.Clear();
+            char salida = 's';
+            while (salida == 's')
+            {
+                Console.Write("Nit: ");
+                clientes[pose].nit = Console.ReadLine();
+                Console.Write("Nombre Cliente: ");
+                clientes[pose].nombre = Console.ReadLine();
+                Console.Write("Apellido Cliente: ");
+                clientes[pose].apellido = Console.ReadLine();
+                Console.Write("Telefono: ");
+                clientes[pose].telefono = Console.ReadLine();
+                Console.Write("Fecha de Nacimiento: ");
+                clientes[pose].fechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+                clientes[pose].edad = (2023 - clientes[pose].fechaNacimiento.Year);
+                pose++;
+                Console.WriteLine("Desea Ingresar otro Cliente S/N ");
+                salida = Convert.ToChar(Console.ReadLine());
+            }
+        }
+
+        public static void MostrarClientes()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(5, 5);
+            Console.Write("Nit");
+            Console.SetCursorPosition(15, 5);
+            Console.Write("Nombre");
+            Console.SetCursorPosition(25, 5);
+            Console.Write("Apellido");
+            Console.SetCursorPosition(35, 5);
+            Console.Write("Telefono");
+            Console.SetCursorPosition(45, 5);
+            Console.Write("Fecha de Nacimiento");
+            Console.SetCursorPosition(65, 5);
+            Console.Write("Edad");
+
+            for (int i = 0; i < pose; i++)
+            {
+                Console.SetCursorPosition(5, (6+i));
+                Console.WriteLine(clientes[i].nit);
+                Console.SetCursorPosition(15, (6+i));
+                Console.WriteLine(clientes[i].nombre);
+                Console.SetCursorPosition(25, (6+i));
+                Console.WriteLine(clientes[i].apellido);
+                Console.SetCursorPosition(35, (6+i));
+                Console.WriteLine(clientes[i].telefono);
+                Console.SetCursorPosition(45, (6+i));
+                Console.WriteLine(clientes[i].fechaNacimiento.ToShortDateString());
+                Console.SetCursorPosition(65, (6+i));
+                Console.WriteLine(clientes[i].edad);
+            }
+            Console.ReadLine();
+        }
+
+        public static void BuscarporNit(String nit)
+        {
+            Console.Clear();
+            int pos = -1;
+            for(int i = 0; i< pose; i++)
+            {
+                if (clientes[i].nit == nit)
+                {
+                    pos = i;
+                }
+            }
+            if(pos != -1)
+            {
+                Console.WriteLine("Nombre Cliente: " + clientes[pos].nombre);
+                Console.WriteLine("Apellido Cliente " + clientes[pos].apellido);
+                Console.WriteLine("Telefono " + clientes[pos].telefono);
+                Console.WriteLine("Fecha de Nacimiento " + clientes[pos].fechaNacimiento.ToShortDateString());
+                Console.WriteLine("Edad " + clientes[pos].edad);
+            }
+            else
+            {
+                Console.WriteLine("El Cliente con este Nit no Existe");
+            }
+            Console.ReadLine();
+        }
+
+        public static void ModificarporNit(String nit)
+        {
+            Console.Clear();
+            int pos = -1;
+            for (int i = 0; i < pose; i++)
+            {
+                if (clientes[i].nit == nit)
+                {
+                    pos = i;
+                }
+                if(pos != -1)
+                {
+                    Console.Write("Nuevo Nombre Cliente: ");
+                    clientes[i].nombre = Console.ReadLine();
+                    Console.Write("Nuevo Apellido Cliente: ");
+                    clientes[i].apellido = Console.ReadLine();
+                    Console.Write("Nuevo Telefono: ");
+                    clientes[i].telefono = Console.ReadLine();
+                    Console.Write("Nueva Fecha de Nacimiento: ");
+                    clientes[i].fechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+                    clientes[i].edad = (2023 - clientes[i].fechaNacimiento.Year);
+                    Console.WriteLine("Cliente Modificado con Exito");
+                }
+                else
+                {
+                    Console.WriteLine("Este Cliente no existe");
+                }
+               
+            }
+            Console.ReadLine();
+        }
+
+        public static void EliminarporNit(String nit)
+        {
+            Console.Clear();
+            Console.Write("Nit a Buscar: ");
+            string nitbuscar = Console.ReadLine();
+            int pos = -1;
+            for (int i = 0; i < pose; i++)
+            {
+                if (clientes[i].nit == nitbuscar)
+                {
+                    pos = i;
+                }
+            }
+            if (pos != -1)
+            {
+                char siono = 's';
+                Console.Write("¿Esta seguro de eliminar este cliente? S/N: ");
+                siono = Convert.ToChar(Console.ReadLine());
+                if (siono == 's' || siono == 'S')
+                {
+                    for (int x = pos; x < pose; x++)
+                    {
+                        clientes[x] = clientes[x + 1];
+                    }
+                    pose--;
+                    Console.WriteLine("Cliente Eliminado con exito");
+                }
+            }
+            else
+                Console.WriteLine("Este cliente no existe");
+        }
+
+       static Cliente[] clientes = new Cliente[10];
+       static int pose = 0;
+
+        public static void Menu()
+        {
+            bool salir = false;
+            String net = "";
+            while (!salir)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Ingresar Empleado");
+                Console.WriteLine("2. Mostrar Empleado");
+                Console.WriteLine("3. Buscar Empleado por Nit");
+                Console.WriteLine("4. Modificar Empleado");
+                Console.WriteLine("5. Eliminar Empleado");
+                Console.WriteLine("6. Salir");
+                Console.WriteLine("Elige una de las opciones");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        IngresarClientes();
+                        break;
+                    case 2:
+                        MostrarClientes();
+                        break;
+                    case 3:
+                        Console.WriteLine("Ingrese el Nit del Cliente que desea buscar");
+                        net = Console.ReadLine();
+                        BuscarporNit(net);
+                        break;
+                    case 4:
+                        Console.WriteLine("Ingrese el Nit del Cliente que desea modificar");
+                        net = Console.ReadLine();
+                        ModificarporNit(net);
+                        break;
+                    case 5:
+                        Console.WriteLine("Ingrese el Nit del Cliente que desea Eliminar");
+                        net = Console.ReadLine();
+                        EliminarporNit(net);
+                        break;
+                    case 6:
+                        Console.WriteLine("Has elegido salir de la aplicación");
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Elige una opcion entre 1 y 5");
+                        break;
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}
